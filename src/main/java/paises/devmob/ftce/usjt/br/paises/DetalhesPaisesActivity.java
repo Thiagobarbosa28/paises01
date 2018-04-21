b43420e8070ec367e905f1672d9c596f2f9a3796
Thiagobarbosa28 <thg.barbosa28@gmail.com>
2018-04-21 00:47:20 -0300

    Initial commit

---
+++ src/main/java/paises/devmob/ftce/usjt/br/paises/DetalhesPaisesActivity.java
@@ -0,0 +1,25 @@
+package paises.devmob.ftce.usjt.br.paises;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by Thiago Barbosa 816117515
+ */
+
+
+
+public class DetalhesPaisesActivity extends Activity {
+    TextView txtNome = (TextView) findViewById(R.id.pais_nome);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_paises);
+        Intent intent = getIntent();
+        String nome = intent.getStringExtra(ListarPaisesActivity.PAIS);
+        txtNome.setText(nome);
+    }
+}
